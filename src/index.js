8990d425d6487afa080fba2fa6ea462e8ac15b67
KennethLG <orsonleiziaga@gmail.com>
2023-05-18 17:12:48 -0500

    "Set auto-commit time interval to 1 minute in index.js file."

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ const projectPath = "C:/Users/DEll/Desktop/Kenneth/SearchingLightProject";
 
 const gitManager = new GitManager(projectPath);
 
+const time = 1000 * 60;
+
 setInterval(() => {
   gitManager.checkForChanges();
-}, 3000);
+}, time);
 
 console.log("AutoCommiter started successfully!");
